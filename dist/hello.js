ecb9ce2c01afc36a5e52dc410e2197e39bd76312
folken718 <folken718@gmail.com>
2019-07-23 00:22:32 -0500

    Adding first complete version

---
+++ dist/hello.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello() {
+    var hello = "hello world!!";
+    console.log(hello);
+    return hello;
+}
+exports.hello = hello;
+//# sourceMappingURL=hello.js.map
